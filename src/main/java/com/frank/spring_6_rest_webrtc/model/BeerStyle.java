04bca7818e3b44bbc2f724770b6812a993628ab3
f.schirrmeister@t-online.de <f.schirrmeister@t-online.de>
2025-04-06 19:00:38 +0200

    First Rest Call

---
+++ src/main/java/com/frank/spring_6_rest_webrtc/model/BeerStyle.java
@@ -0,0 +1,5 @@
+package com.frank.spring_6_rest_webrtc.model;
+
+public enum BeerStyle {
+        LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
---
+++ src/main/java/com/frank/spring_6_rest_webrtc/service/BeerService.java
@@ -0,0 +1,20 @@
+package com.frank.spring_6_rest_webrtc.service;
+
+import com.frank.spring_6_rest_webrtc.model.Beer;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface BeerService {
+    List<Beer> listBeers();
+
+    Beer getBeerById(UUID id);
+
+    Beer saveNewBeer(Beer beer);
+
+    void updateBeerById(UUID beerId, Beer beer);
+
+    void deleteById(UUID beerId);
+
+    void patchBeerById(UUID beerId, Beer beer);
+}
