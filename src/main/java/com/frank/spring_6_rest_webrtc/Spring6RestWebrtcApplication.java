b1cdf5b5cd65a7fb1f0694169862ef8a1b68822f
f.schirrmeister@t-online.de <f.schirrmeister@t-online.de>
2025-04-06 13:33:37 +0200

    initial commit

---
+++ src/main/java/com/frank/spring_6_rest_webrtc/Spring6RestWebrtcApplication.java
@@ -0,0 +1,13 @@
+package com.frank.spring_6_rest_webrtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6RestWebrtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6RestWebrtcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frank/spring_6_rest_webrtc/Spring6RestWebrtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frank.spring_6_rest_webrtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6RestWebrtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
